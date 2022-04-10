4ad1b17e5115cab78073f652bc11e5429f1d8efe
martin0330 <martin.cruz0330@yahoo.com>
2022-04-10 10:56:10 -0500

    created functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,37 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: 'name', 
+        type: 'input',
+        message: 'What is your name?'
+    },
+    {
+        name: 'gitHubName',
+        type: 'input',
+        message: 'What is your GitHub name?'
+    },
+    {
+        
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(answerData => {
+        writeToFile("NewREADME.md", generateMarkdown(answerData));
+    })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,29 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') return '[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)';
+  if (license === 'GPLv3') return '[![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)](http://perso.crans.org/besson/LICENSE.html)'
+  if (license === 'none') return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') return 'https://opensource.org/licenses/MIT'
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  renderLicenseBadge(license);
+  renderLicenseLink(license);
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `# ${data.name}
+  ## ${data.gitHubName}
+  ## ${renderLicenseSection(data.license)}
 `;
 }
 
